2df7f9402fb891f48dcbc987b72309bfac2b2429
Mehedihasanb <714635@student.inholland.nl>
2025-10-09 03:52:14 +0200

    Add project files.

---
+++ NoDesk Group1/Repositories/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using NoDesk_Group1.Repositories.Interfaces;
+using NoDeskGroup1.Models;
+
+namespace NoDeskGroup1.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IMongoCollection<Employee> _col;
+        public EmployeeRepository(IMongoDatabase db) => _col = db.GetCollection<Employee>("employees");
+
+        public async Task<IEnumerable<Employee>> GetAllAsync() => await _col.Find(_ => true).ToListAsync();
+        public async Task<Employee?> GetByIdAsync(string id) => await _col.Find(x => x.Id == ObjectId.Parse(id)).FirstOrDefaultAsync();
+        public async Task AddAsync(Employee m) => await _col.InsertOneAsync(m);
+        public async Task UpdateAsync(string id, Employee m) => await _col.ReplaceOneAsync(x => x.Id == ObjectId.Parse(id), m);
+        public async Task DeleteAsync(string id) => await _col.DeleteOneAsync(x => x.Id == ObjectId.Parse(id));
+    }
+}
---
+++ NoDesk Group1/Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using NoDeskGroup1.Models;
+
+namespace NoDesk_Group1.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(string id);
+        Task AddAsync(Employee model);
+        Task UpdateAsync(string id, Employee model);
+        Task DeleteAsync(string id);
+    }
+}
