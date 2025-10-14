cfd9bf0118b72860ba1ddb29f1554267ac03a843
Majd <55513222+MajdAlsayed@users.noreply.github.com>
2025-10-14 11:08:57 +0200

    Delete NoDesk Group1 directory

--- NoDesk Group1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NoDesk_Group1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
